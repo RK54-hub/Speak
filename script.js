7313565e3d9bf6066e52f66dfdef87266e00454b
RK54-hub <rajkumar.senthilvel@gmail.com>
2025-07-15 19:23:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        function soeak(){
+        function speak(){
             const input = document .getElementById("text").value;
             const utterance = new SpeechSynthesisUtterance(input);
             speechSynthesis.speak(utterance);
